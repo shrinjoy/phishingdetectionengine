7f94406b8b44e9c5f76e490dfea438dfa636eaf4
Noiz <shrinjoybiswas0@gmail.com>
2025-04-06 10:22:18 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from levelone_check import check
+from levelone_check import emailpayload
 app = FastAPI()
-
-
-
-
-
 @app.get("/")
 def root():
     return({"message":"welcome to root"})
 
+@app.post("/check/")
+def startemailcheck(payload:emailpayload):
+    return check(payload)
---
+++ levelone_check.py
@@ -0,0 +1,50 @@
+from typing import Union
+from pydantic import BaseModel
+from enum import Enum
+import json
+
+
+
+class emailpayload(BaseModel):
+    emailbody:str
+    emailsender:str
+
+
+
+
+
+class threatlevel(Enum):
+    good="good"
+    sus="sus"
+    bad="bad"
+
+
+class checkstatus(BaseModel):
+    status:threatlevel
+    suswords:list[str]
+
+
+
+
+
+def check(data:emailpayload):
+
+    
+    emailid = data.emailsender
+    emailbody  = data.emailbody.split(" ")
+    suswordjson =json.load(open("suswords.json","r"))
+    suswords =suswordjson["words"]
+    wordsfound = []
+    for word in emailbody:
+        if word in suswords:
+            print("found sus word")
+            wordsfound.append(word)
+
+    suswordscount = len(wordsfound)
+    if(suswordscount>0 and suswordscount<3):
+        return checkstatus(status=threatlevel.sus,suswords=wordsfound)
+    elif(suswordscount>3):
+        return checkstatus(status=threatlevel.bad,suswords=wordsfound)
+    else:
+        return checkstatus(status=threatlevel.good,suswords=wordsfound)
+
